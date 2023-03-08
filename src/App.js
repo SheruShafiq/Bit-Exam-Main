d86d31ad0c98500193cce6e8b7200d945e1d74cc
Sheru <100682913+SheruShafiq@users.noreply.github.com>
2023-03-08 16:56:27 +0100

    figured out an email sending service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import emailjs from "@emailjs/browser"
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
 
 function App() {
+  const sendEmail = (e) => {
+    e.preventDefault();
+  emailjs.sendForm('service_nh0au8c',  'template_k07o36l', e.target, 'WaOT6BUTbyQbhGR8B')
+  console.log("Email sent")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="page__title">Contact Us</h1>
+      <form className="contact__form" onSubmit={sendEmail}>
+        <label htmlFor="emailFrom">Email:</label>
+        <input type="text" name="email_from" id="emailFrom" className="email__from" placeholder="person@example.com"/>
+        <label htmlFor="message">Message:</label>
+        <textarea name="message" id="message" className="message__box"></textarea>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
