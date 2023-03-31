31cbd73758ce3f53edca9faade033f5b0966f0a8
Sheru <humaira.laeeq@gmail.com>
2023-03-31 19:01:21 +0200

    Refactoring and spliting Form component

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,25 +1,43 @@
-//This code is used to send an email to the user when they submit the contact form
-//EmailJS is used to send the email
-//The email is sent to the user's email address
-//The email is sent to the user's name
-//The email is sent to the user's message
-
 import React, { useState } from "react";
-import emailjs from "@emailjs/browser";
+import emailjs from "emailjs/browser";
 import "../styles/FormStyles.css";
 import TextField from "@mui/material/TextField";
 import { useNavigate } from "react-router-dom";
 import CountUp from "react-countup";
+import { textFieldProps } from "./textfieldProps";
 
+// Component to handle the contact form
 function FormComponent() {
   const navigate = useNavigate();
-  const [error, setError] = React.useState("");
-  const [isLoading, SetIsLoading] = React.useState(false);
-  const handleButtonClick = () => {};
+  const [error, setError] = useState(""); // state to handle error messages
+  const [isLoading, setIsLoading] = useState(false); // state to handle loading status
+  const handleButtonClick = () => {}; // function to handle the button click event
+  // Function to handle the form submit event
 
+  const handleFormSubmit = async (e) => {
+    e.preventDefault(); // prevent the default form submission
+    setIsLoading(true); // set loading to true
+    try {
+      // use emailjs to send the email
+      await emailjs.sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      );
+      // wait for 3 seconds to simulate delay
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      console.log("Email sent");
+      setIsLoading(false); // set loading to false
+      navigate("/result", { error: error }); // navigate to the result page
+    } catch (error) {
+      setError(error); // set the error message
+      navigate("/result", { error: error }); // navigate to the result page
+    }
+  };
   const sendEmail = async (e) => {
     e.preventDefault();
-    SetIsLoading(true);
+    setIsLoading(true);
     try {
       await emailjs.sendForm(
         "service_nh0au8c",
@@ -29,7 +47,7 @@ function FormComponent() {
       );
       await new Promise((resolve) => setTimeout(resolve, 3000)); // 3 sec
       console.log("Email sent");
-      SetIsLoading(false);
+      setIsLoading(false);
       setError("error");
       navigate("/result", { error: error });
     } catch (error) {
@@ -37,119 +55,18 @@ function FormComponent() {
       navigate("/result", { error: error });
     }
   };
-  const textFieldProps = [
-    { id: "name-field", type: "text", name: "name", label: "Naam" },
-    {
-      id: "date-field",
-      type: "date",
-      name: "date",
-      label: "Geboortedatum",
-      focused: true,
-      sx: {
-        "& .MuiInputBase-input": {
-          textAlignLast: "center",
-        },
-
-        width: "100%",
-        marginTop: "2rem",
-        color: "white",
-        "& .MuiInput-underline:after": {
-          borderBottomColor: " rgb(255,255,255, 0.1)",
-          borderBottomWidth: "1px",
-        },
-        "& .MuiInput-underline:before": {
-          borderBottomWidth: "1px",
-          borderBottomColor: " rgb(255,255,255, 0.3)",
-        },
-      },
-      InputLabelProps: {
-        style: { fontFamily: "Poppins", color: "white", fontSize: "20px" },
-      },
-      inputProps: {
-        style: {
-          fontFamily: "Poppins",
-          color: "white",
-          marginTop: "1rem",
-        },
-      },
-    },
-    { id: "email-field", type: "email", name: "email", label: "Email" },
-    { id: "tel-field", type: "tel", name: "tel", label: "Telefoon" },
-    { id: "github-field", type: "url", name: "github", label: "GitHub" },
-    { id: "linkdin-field", type: "url", name: "linkdin", label: "Linkdin" },
-    {
-      id: "portfolio-field",
-      type: "url",
-      name: "portfolio",
-      label: "Portfolio website",
-    },
-    {
-      id: "opleiding-field",
-      type: "text",
-      name: "Opleiding",
-      label: "Opleiding",
-    },
-    {
-      id: "solicitatiebrief-field",
-      type: "file",
-      name: "Solicitatie brief",
-      label: "Solicitatie brief",
-      sx: {
-        "& .MuiInputBase-input": {
-          textAlignLast: "end",
-        },
-        width: "100%",
-        marginTop: "1rem",
-        color: "white",
-        "& .MuiInput-underline:after": {
-          borderBottomColor: " rgb(255,255,255, 0.1)",
-          borderBottomWidth: "1px",
-        },
-        "& .MuiInput-underline:before": {
-          borderBottomWidth: "1px",
-          borderBottomColor: " rgb(255,255,255, 0.3)",
-        },
-      },
-    },
-    {
-      id: "cv-upload-field",
-      type: "file",
-      name: "CV uploaden",
-      label: "CV uploaden",
-      sx: {
-        "& .MuiInputBase-input": {
-          textAlignLast: "end",
-        },
-        width: "100%",
-        marginTop: "1rem",
-        color: "white",
-        "& .MuiInput-underline:after": {
-          borderBottomColor: " rgb(255,255,255, 0.1)",
-          borderBottomWidth: "1px",
-        },
-        "& .MuiInput-underline:before": {
-          borderBottomWidth: "1px",
-          borderBottomColor: " rgb(255,255,255, 0.3)",
-        },
-      },
-    },
-    {
-      id: "found-us-field",
-      type: "textarea",
-      name: "Hoe heb je ons gevonden?",
-      label: "Hoe heb je ons gevonden?",
-    },
-  ];
-
+  // Divide the text fields into two equal halves
   const half = Math.ceil(textFieldProps.length / 2);
+  const textFieldPropsIn = textFieldProps;
+  // Render the component
   return (
     <>
       <div className="FormDesktop">
-        <form className="contact__form" onSubmit={sendEmail}>
+        <form className="contact__form" onSubmit={handleFormSubmit}>
           <div id="firstHalf">
+            {/* Render the first half of the text fields */}
             {textFieldProps.slice(0, half).map((textField) => (
               <TextField
-                // x
                 key={textField.id}
                 variant="standard"
                 placeholder=""
@@ -170,6 +87,42 @@ function FormComponent() {
                   marginTop: "1rem",
                   color: "white",
                   "& .MuiInput-underline:after": {
+                    borderBottomColor: "rgb(255, 255, 255, 0.3)",
+                    borderBottomWidth: "1px",
+                  },
+                  "& .MuiInput-underline:before": {
+                    borderBottomWidth: "1px",
+                    borderBottomColor: "rgb(255, 255, 255, 0.3)",
+                  },
+                }}
+                {...textField}
+              />
+            ))}
+          </div>
+          <div id="secondHalf">
+            {/* Render the second half of the text fields */}
+            {textFieldProps.slice(half).map((textField) => (
+              <TextField
+                key={textField.id}
+                variant="standard"
+                placeholder=""
+                InputLabelProps={{
+                  style: { fontFamily: "Poppins", color: "white" },
+                }}
+                inputProps={{
+                  style: {
+                    fontFamily: "Poppins",
+                    color: "white",
+                    marginTop: "0.5rem",
+                  },
+                }}
+                sx={{
+                  input: { color: "white" },
+                  width: "768px",
+
+                  marginTop: "1rem",
+                  color: "white",
+                  "& .MuiInput-underline:after": {
                     borderBottomColor: " rgb(255,255,255, 0.3)",
                     borderBottomWidth: "1px",
                   },
@@ -183,7 +136,7 @@ function FormComponent() {
             ))}
           </div>
           <div id="secondHalf">
-            {textFieldProps.slice(half).map((textField) => (
+            {textFieldPropsIn.slice(half).map((textField) => (
               <TextField
                 // x
                 key={textField.id}
@@ -236,7 +189,7 @@ function FormComponent() {
       </div>
       <div className="FormMobile">
         <form className="contact__form" onSubmit={sendEmail}>
-          {textFieldProps.map((textField) => (
+          {textFieldPropsIn.map((textField) => (
             <TextField
               // x
               key={textField.id}
